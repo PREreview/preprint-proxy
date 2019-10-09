843d516c6b228268c294fbf808c11265758b50f3
Rik Smith-Unna <rik@fathomlabs.io>
2019-10-09 18:52:58 +0100

    Add run script

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '67.207.92.73'
-
-// Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 443
 
 var cors_proxy = require('cors-anywhere')
 
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins for now
+    originWhitelist: [],
     httpsOptions: require('./ssl.js')
 }).listen(port, host, function() {
     console.log('Running preprint proxy on ' + host + ':' + port)
